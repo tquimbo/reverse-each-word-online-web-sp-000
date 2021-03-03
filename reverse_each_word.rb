baa9d493fd5e589c70c3025b960d803b5b00879a
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 01:17:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@
 #reverse_each_word of a sentence with a string argurment
 #first find a method that reveres a word in ruby
 def reverse_each_word(array)
-  array.split(" ").collect {|x| x.reverse!}
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
 end
